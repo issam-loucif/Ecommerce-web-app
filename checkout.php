38c2d40ac87999c5a02acb482b9d6a9b1fd33601
loucif issam <issamloucif@outlook.com>
2021-05-25 23:52:46 +0100

    add contact

--- checkout.php
+++ checkout.php
@@ -8,26 +8,23 @@ include('includes/header.php');
                         <div class="checkout_details_area mt-50 clearfix">
 
                             <div class="cart-title">
-                                <h2>Checkout</h2>
+                                <h2>Contact US</h2>
                             </div>
 
-                            <form action="#" method="post">
+                            <form action="mail.php" method="post">
                                 <div class="row">
                                     <div class="col-md-6 mb-3">
-                                        <input type="text" class="form-control" id="first_name" value="" placeholder="First Name" required>
+                                        <input type="text" class="form-control" name="name" value="" placeholder="First Name" required>
                                     </div>
                                     <div class="col-md-6 mb-3">
-                                        <input type="text" class="form-control" id="last_name" value="" placeholder="Last Name" required>
+                                        <input type="text" class="form-control" name="name" value="" placeholder="Last Name" required>
                                     </div>
                                     <div class="col-12 mb-3">
-                                        <input type="text" class="form-control" id="company" placeholder="Company Name" value="">
+                                        <input type="email" class="form-control" name="email" placeholder="Email" value="">
                                     </div>
-                                    <div class="col-12 mb-3">
-                                        <input type="email" class="form-control" id="email" placeholder="Email" value="">
-                                    </div>
-                                    <div class="col-12 mb-3">
+                                    <div class="col-6 mb-3">
                                         <select class="w-100" id="country">
-                                        <option value="usa">United States</option>
+                                        <option value="usa">Maroc</option>
                                         <option value="uk">United Kingdom</option>
                                         <option value="ger">Germany</option>
                                         <option value="fra">France</option>
@@ -37,32 +34,13 @@ include('includes/header.php');
                                         <option value="cana">Canada</option>
                                     </select>
                                     </div>
-                                    <div class="col-12 mb-3">
-                                        <input type="text" class="form-control mb-3" id="street_address" placeholder="Address" value="">
-                                    </div>
-                                    <div class="col-12 mb-3">
-                                        <input type="text" class="form-control" id="city" placeholder="Town" value="">
-                                    </div>
                                     <div class="col-md-6 mb-3">
-                                        <input type="text" class="form-control" id="zipCode" placeholder="Zip Code" value="">
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <input type="number" class="form-control" id="phone_number" min="0" placeholder="Phone No" value="">
+                                        <input type="number" class="form-control" name="phone" min="0" placeholder="Phone No" value="">
                                     </div>
                                     <div class="col-12 mb-3">
-                                        <textarea name="comment" class="form-control w-100" id="comment" cols="30" rows="10" placeholder="Leave a comment about your order"></textarea>
+                                        <textarea name="message" class="form-control w-100" id="message" cols="30" rows="10" placeholder="Leave a message"></textarea>
                                     </div>
-
-                                    <div class="col-12">
-                                        <div class="custom-control custom-checkbox d-block mb-2">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck2">
-                                            <label class="custom-control-label" for="customCheck2">Create an accout</label>
-                                        </div>
-                                        <div class="custom-control custom-checkbox d-block">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck3">
-                                            <label class="custom-control-label" for="customCheck3">Ship to a different address</label>
-                                        </div>
-                                    </div>
+                                    <button class="btn amado-btn mb-15 " type="submit">Send</button>
                                 </div>
                             </form>
                         </div>
